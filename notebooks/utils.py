e86a9f792508b7eba02ae616557907d1ad636b66
tzny <tizonyves@gmail.com>
2022-06-03 19:30:22 +0200

    A library example

---
+++ notebooks/utils.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+print("he importado esta libreria")
+
+def print_version():
+    """
+    Aqui puedo comentar mi codigo libremente y no se va a ejecutar nada
+    entre las triples comillas.
+    """
+    print("version 0")
+    print(f"Numpy version: {np.__version__}")
